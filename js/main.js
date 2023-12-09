ddbdc07a0b3f22ad7b3ed826546fa5c695f2aa78
santiagogonzalezgomila <sgonzalezgomila@gmail.com>
2023-12-09 19:53:56 -0300

    segunda pre entrega

--- js/main.js
+++ js/main.js
@@ -40,3 +40,68 @@ function inicioDeSesion() {
 alert("Bienvenido, regístrese e inicie sesión para continuar");
 registro();
 inicioDeSesion();
+
+function Producto(nombre, talle, precio) {
+    this.nombre = nombre;
+    this.talle = talle;
+    this.precio = precio;
+}
+
+function Catalogo() {
+    this.catalogo = [];
+
+    this.agregarProducto = function () {
+        let nombre = prompt("Ingrese el nombre del producto");
+        let talle = prompt("Ingrese el talle del producto");
+        let precio = prompt("Ingrese el precio del producto");
+
+        precio = parseInt(precio);
+
+        if (isNaN(precio)) {
+            console.log("Por favor, ingrese un precio válido en números.");
+        } else {
+            let nuevoProducto = new Producto(nombre, talle, precio);
+            this.catalogo.push(nuevoProducto);
+        }
+    };
+
+    this.cargarProductos = function () {
+        let respuesta;
+        do {
+            this.agregarProducto();
+            respuesta = prompt("¿Desea agregar otro producto? (sí/no)").toLowerCase();
+        } while (respuesta === "si");
+    };
+
+    this.mostrarCatalogo = function () {
+        console.log("Productos en el catálogo:");
+        this.catalogo.forEach(function (producto) {
+            console.log(`Nombre: ${producto.nombre}, Talle: ${producto.talle}, Precio: ${producto.precio}`);
+        });
+    };
+
+    this.sumarPrecios = function () {
+        let respuestaSumar = prompt("¿Desea sumar los precios de los productos? (sí/no)").toLowerCase();
+
+        if (respuestaSumar === "si") {
+            let total = this.catalogo.reduce(function (acumulador, producto) {
+                return acumulador + producto.precio;
+            }, 0);
+
+            console.log(`El total de los precios es: ${total}`);
+        } else {
+            console.log("No se sumaron los precios.");
+        }
+    };
+}
+
+
+let miCatalogo = new Catalogo();
+
+
+miCatalogo.cargarProductos();
+
+miCatalogo.mostrarCatalogo();
+
+miCatalogo.sumarPrecios();
+
